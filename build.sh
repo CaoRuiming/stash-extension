930181feb468b6e55c058ba86b99ef83102e6d22
Raymond Cao <raymondcao@Raymonds-MacBook-Air.local>
2023-04-07 12:25:16 -0700

    Add Firefox support

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-npm ci --only=production && npm run build
+if [ ! -d "node_modules" ]; then
+    echo "Installing dependenies..."
+    npm ci --only=production
+fi
+
+echo "Running build"
+npx tsc
+
+rm -f extension/manifest.json
+if [[ "$1" == "firefox" ]]; then
+    echo "Building for Firefox"
+    cp src/manifest/firefox-manifest.json extension/manifest.json
+else
+    cp src/manifest/chrome-manifest.json extension/manifest.json
+fi
